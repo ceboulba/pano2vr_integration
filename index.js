6e0cbcf414b2c0e0a075dfeda1e1793ecf7419ef
ceboulba <antoine@archipicture.fr>
2020-04-19 01:43:49 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+
+//import other files
+import "./pano2vr_player.js";
+import {pano2vrSkin} from "./skin.js";
+import"./webvr/three.min.js";
+import "./webvr/webvr-polyfill.min.js";
+
+const container = document.querySelector("#container");
+
+const pano = new pano2vrPlayer("container");
+const skin = new pano2vrSkin(pano);
+
+		window.addEventListener("load", function() {
+				pano.readConfigUrlAsync("./pano.xml");
+			});  
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
